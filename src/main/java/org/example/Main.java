c02586fa8cf3499c63c162f94fbe1f456be4354e
Sergey Koryushov <lala170392@mail.ru>
2023-04-11 22:50:45 +0300

    'a'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now());
+        System.out.println();
     }
 }
