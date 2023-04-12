71fad6a6de0a4be9cb5bc36e17ab4894c0df4cac
Sergey Koryushov <lala170392@mail.ru>
2023-04-12 10:10:26 +0300

    'cha'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now());
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             System.out.println(i);
         }
     }
