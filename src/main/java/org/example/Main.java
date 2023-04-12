045d22148314e6cf40aaa1df9c61daa5347601aa
Sergey Koryushov <lala170392@mail.ru>
2023-04-12 10:01:34 +0300

    'for'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,8 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now());
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
     }
 }
