4bd07a8f6e2555f19d6b54ee3fde3981c0567dbc
Sergeykoryushov <125565652+Sergeykoryushov@users.noreply.github.com>
2023-04-12 10:34:14 +0300

    Update and rename Src/main/java/org/example/Main.java to Src/Главная/java/org/пример/Main.java

--- Src/Главная/java/org/пример/Main.java
+++ Src/Главная/java/org/пример/Main.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now());
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println(i);
             System.out.println(i*2);
         }
