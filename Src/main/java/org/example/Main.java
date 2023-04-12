c5a72c638fe285d45b1bde3bc5cc1e2adf179fac
Sergey Koryushov <lala170392@mail.ru>
2023-04-12 22:45:53 +0300

    'saa'

--- Src/main/java/org/example/Main.java
+++ Src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now());
         for (int i = 0; i < 4; i++) {
-
             System.out.println(i*5);
         }
     }
