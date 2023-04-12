0fafd38758ada3731842d2d26f46b78dfa245883
Sergeykoryushov <125565652+Sergeykoryushov@users.noreply.github.com>
2023-04-12 10:44:13 +0300

    Update Main.java

--- Src/main/java/org/example/Main.java
+++ Src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now());
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 4; i++) {
             System.out.println(i);
             System.out.println(i*8);
         }
