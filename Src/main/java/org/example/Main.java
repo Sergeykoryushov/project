135a7ac1f8fa7f13076567e11476f3c588e13757
Sergey Koryushov <lala170392@mail.ru>
2023-04-13 12:24:07 +0300

    'd'

--- Src/main/java/org/example/Main.java
+++ Src/main/java/org/example/Main.java
@@ -5,11 +5,12 @@ import java.time.LocalDateTime;
 public class Main {
 
     public static final String NUMBER = "89274994878";
-    
+
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now());
         for (int i = 0; i < 4; i++) {
             System.out.println(i*7);
+            System.out.println(i);
         }
     }
 }
