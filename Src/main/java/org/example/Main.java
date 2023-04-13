ceef36d86cd97f7147c3847542fee09bf3fbf9cd
Sergey Koryushov <lala170392@mail.ru>
2023-04-13 11:35:32 +0300

    'constant'

--- Src/main/java/org/example/Main.java
+++ Src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import java.time.LocalDateTime;
 
 public class Main {
+    public static final String NUMBER = "89274994878";
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now());
         for (int i = 0; i < 4; i++) {
